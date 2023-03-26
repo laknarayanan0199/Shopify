9d848e8ef1080dfadc70e1dd461f9331115a8ee6
laknarayanan0199 <laknarayanan0199@gmail.com>
2023-03-26 17:53:57 +0530

    fixed bugs

--- src/components/UI/MainHeader.jsx
+++ src/components/UI/MainHeader.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { uiActions } from "../../store/uiSlice";
 
 import CartButton from "../cart/CartButton";
 import SearchPage from "../pages/Search";
 import classes from "../UI/Header.module.css";
 
 const MainHeader = () => {
-  const dispatch = useDispatch();
-  const homePageHandler = () => {
-    dispatch(uiActions.showProducts(false));
-  };
-
   return (
     <nav className={classes.header}>
       <h1>
-        <Link to={"/"} onClick={homePageHandler}>
-          SS
-        </Link>
+        <Link to={"/"}>SS</Link>
       </h1>
       <ul>
         <li className={classes.listItems}>
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 import productsSlice from "./productsSlice";
-import uiSlice from "./uiSlice";
 
 const store = configureStore({
   reducer: {
-    ui: uiSlice.reducer,
     products: productsSlice.reducer,
     cart: cartSlice.reducer,
   },
